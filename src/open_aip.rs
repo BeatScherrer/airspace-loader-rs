584a6bec0ddf119bd70e95cdf09dc50006c49c39
Beat Scherrer <beat.scherrer@gmail.com>
2021-08-15 14:39:59 +0200

    progress on polygon visitor implementation

--- src/open_aip.rs
+++ src/open_aip.rs
@@ -1,13 +1,16 @@
 // use serde::de::{self, Deserialize, Deserializer, Visitor};
 use serde::Deserialize;
+use serde::Deserializer;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename = "OPENAIP")]
 pub struct OpenAip {
   #[serde(rename = "VERSION")]
   version: String,
+
   #[serde(rename = "DATAFORMAT")]
   data_format: f32,
+
   #[serde(rename = "AIRSPACES")]
   airspaces: Airspaces,
 }
@@ -33,17 +36,23 @@ pub struct Airspace {
 
   #[serde(rename = "NAME")]
   pub name: String,
-  // #[serde(rename = "ALTLIMIT_TOP")]
-  // pub upper: AltLimit,
 
-  // #[serde(rename = "ALTLIMIT_BOTTOM")]
-  // pub lower: AltLimit,
+  #[serde(rename = "ALTLIMIT_TOP")]
+  pub upper: AltLimit,
+
+  #[serde(rename = "ALTLIMIT_BOTTOM")]
+  pub lower: AltLimit,
+
+  #[serde(rename = "GEOMETRY")]
+  geometry: Polygon,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct AltLimit {
   #[serde(rename = "ALT")]
   altitude: Altitude,
+
+  #[serde(rename = "REFERENCE")]
   reference: AltitudeReference,
 }
 
@@ -53,10 +62,19 @@ pub enum AltitudeReference {
 }
 
 #[derive(Debug, Deserialize, Clone)]
-pub enum Altitude {
-  FL(u32),
-  M(u32),
-  FT(u32),
+pub struct Altitude {
+  #[serde(rename = "UNIT")]
+  unit: AltitudeUnit,
+
+  #[serde(rename = "$value")]
+  value: u32,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+enum AltitudeUnit {
+  FL,
+  FT,
+  M,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -64,9 +82,71 @@ pub enum AirspaceCategory {
   WAVE,
 }
 
+#[derive(Debug, Clone)]
+pub struct Polygon {
+  // #[serde(rename = "POLYGON")]
+  point: Vec<(f32, f32)>,
+  // point: String,
+}
+
 impl OpenAip {
   pub fn get_airspaces(&self) -> Vec<Airspace> {
     self.airspaces.airspaces.clone()
+  }
+}
+
+use serde::de::{self, Visitor};
+use std::fmt;
+
+struct PolygonVisitor {}
+
+// Define the polygon visitor
+impl PolygonVisitor {
+  fn new() -> Self {
+    PolygonVisitor {}
+  }
+
+  fn points_from_string(string: &str) -> Vec<(f32, f32)> {
+    let point: (f32, f32);
+
+    // let string_points: Vec<&str> = string.split(",").collect();
+    string.split(",").map(|s| println!("{}", s));
+
+    vec![(1.0, 2.0)]
+  }
+}
+
+impl<'de> Visitor<'de> for PolygonVisitor {
+  type Value = (f32, f32);
+
+  fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+    formatter.write_str("")
+  }
+
+  fn visit_string<E>(self, value: String) -> Result<Self::Value, E>
+  where
+    E: de::Error,
+  {
+    // TODO map the string to (f32, f32)
+    Ok((1.0, 2.0))
+  }
+
+  fn visit_bytes<E>(self, value: &[u8]) -> Result<Self::Value, E> {
+    Ok((3.0, 4.0))
+  }
+
+  fn visit_seq<A>(self, seq: A) -> Result<Self::Value, E> {
+    Ok((5.0, 6.0))
+  }
+}
+
+// drive the visitor
+impl<'de> Deserialize<'de> for Polygon {
+  fn deserialize<D>(deserializer: D) -> Result<Polygon, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    deserializer.deserialize_any(PolygonVisitor)
   }
 }
 
